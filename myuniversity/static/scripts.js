8b62a3f54a34157c4473267b365d6567a39cc41a
Rishav Raj <101454458+starsailor2@users.noreply.github.com>
2024-05-19 19:16:20 +0000

    1st attempt

---
+++ myuniversity/static/scripts.js
@@ -0,0 +1,5 @@
+// static/js/scripts.js
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("JavaScript is loaded and ready to go!");
+    // Add any JavaScript functionality here
+});
