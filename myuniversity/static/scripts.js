c8021e6a51191bada7810bbe1b334811ad7ce50d
Rishav Raj <101454458+starsailor2@users.noreply.github.com>
2024-05-22 02:36:08 +0000

    delete

--- myuniversity/static/scripts.js
+++
@@ -1,5 +0,0 @@
-// static/js/scripts.js
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("JavaScript is loaded and ready to go!");
-    // Add any JavaScript functionality here
-});
--- todo-list/database.js
+++
@@ -1,9 +0,0 @@
-// database.js
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:');
-
-db.serialize(() => {
-    db.run("CREATE TABLE tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, task TEXT, done BOOLEAN)");
-});
-
-module.exports = db;
